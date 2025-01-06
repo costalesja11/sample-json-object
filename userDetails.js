ebad99e849e08fcf9ce0f0f7ac52134fb56f569f
costalesja11 <jcostales011@gmail.com>
2025-01-06 18:23:37 +0800

    Create a dummy json object data

---
+++ userDetails.js
@@ -0,0 +1,17 @@
+[
+    {
+        "name": "Hello Test1",
+        "age": 25,
+        "nationality": "Filipino"
+    },
+    {
+        "name": "Hello Test2",
+        "age": 30,
+        "nationality": "Chinese"
+    },
+    {
+        "name": "Hello Test3",
+        "age": 35,
+        "nationality": "Filipino"
+    }
+]
